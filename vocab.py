6a85d613d049b9c1ef23904b04de332aa47008ca
Nebelhom <nebelhom@hotmail.com>
2019-02-14 22:13:07 +0100

    Create vocab.py
    vocab.py simple file to load csv file called vocab.csv.
    Uses format Language1,Language2,<filter flags, unlimited>

---
+++ vocab.py
@@ -0,0 +1,29 @@
+#!usr/bin/env python
+
+# CLI Vocab Trainer for Russian Vocabulary
+# Just showing the word and asking what it means etc.
+
+# For German to Russian, just show the word and then the rest
+# Russian to German: Randomly pick complete or incomplete aspect
+
+# When clearing up, show the entire row
+
+import csv
+
+def read_csv(fname, delimiter=';', encoding='utf-8'):
+    """Reads in vocabulary from custom csv file and returns as list"""
+
+    vocab = []
+
+    with open(fname, newline='', encoding=encoding) as csvfile:
+        csvreader = csv.reader(csvfile, delimiter=delimiter)
+        for row in csvreader:
+            vocab.append(row)
+    return vocab
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
